b6d838d1294555e01d9e8722aee4bad62a27e83e
Phillip Manwaring <phillip.manwaring@gmail.com>
2018-07-02 15:03:23 -0400

    add ci config

--- stacks/check.ts
+++ stacks/check.ts
@@ -1,4 +1,4 @@
-import {  CloudWatchLogsEvent, Callback, Context, Handler } from 'aws-lambda';
+import { CloudWatchLogsEvent, Callback, Context, Handler } from 'aws-lambda';
 import { CloudFormation, SNS } from 'aws-sdk';
 import * as log from 'winston';
 log.configure({ level: process.env.LOG_LEVEL });
@@ -11,56 +11,60 @@ export const handler: Handler = (event: CloudWatchLogsEvent, context: Context, c
   log.info('Odin is now checking to see if any stacks are worthy of entering Valhalla');
 
   listAllStacks()
-    .then( stacks => getStacksToDelete(stacks, event))
-    .then( stacks => publishStacksForDeletion(stacks, event))
-    .then( () => callback(null, 'Finished checking stacks for deletion'))
-    .catch( err => callback(err));
+    .then(stacks => getStacksToDelete(stacks, event))
+    .then(stacks => publishStacksForDeletion(stacks, event))
+    .then(() => callback(null, 'Finished checking stacks for deletion'))
+    .catch(err => callback(err));
 };
 
 function listAllStacks(): Promise<any> {
   const params = {};
   return cloudFormation.describeStacks(params).promise();
-};
+}
 
 function getStacksToDelete(response, config): Promise<any> {
   log.debug('Received list stacks response', response);
-  return Promise.resolve( response.Stacks.filter( stack => shouldDeleteStack(stack, config) ));
-};
+  return Promise.resolve(response.Stacks.filter(stack => shouldDeleteStack(stack, config)));
+}
 
 function shouldDeleteStack(stack, config): boolean {
   log.debug('Seeing if stack should be deleted', stack);
-  return stackIsNonProdOrAutomation(stack, config)
-      && stackIsStale(stack, config)
-      && stackIsInDeletableStatus(stack, config);
-};
+  return (
+    stackIsNonProdOrAutomation(stack, config) && stackIsStale(stack, config) && stackIsInDeletableStatus(stack, config)
+  );
+}
 
-// Stack doesn't have a stage tag or tag isn't production/automation
+// Stack doesn't have a stage tag or tag isn't in whitelisted config
 function stackIsNonProdOrAutomation(stack, config): boolean {
   const stage = stack.Tags.find(tag => tag.Key.toUpperCase() === 'STAGE');
   const isNonProdOrAutomation = !stage || config.stagesToRetain.indexOf(stage.Value.toUpperCase()) < 0;
-  log.debug(`Stack stage is ${stage ? stage.Value : 'undefined'}, which ${isNonProdOrAutomation ? 'isn\'t' : 'is'} production or automation`);
+  log.debug(
+    `Stack stage is ${stage ? stage.Value : 'undefined'}, which ${
+      isNonProdOrAutomation ? "isn't" : 'is'
+    } production or automation`
+  );
   return isNonProdOrAutomation;
-};
+}
 
 // Stack hasn't been updated recently - last updated setting configured in CloudWatch alarm, set in serverless.yml
 function stackIsStale(stack, config): boolean {
   const stackLastUpdated = stack.LastUpdatedTime ? stack.LastUpdatedTime : stack.CreationTime;
-  const lastUpdated = Math.floor((<any> (new Date()) - stackLastUpdated) / 36e5);
+  const lastUpdated = Math.floor((<any>new Date() - stackLastUpdated) / 36e5);
   const isStale = lastUpdated >= parseInt(config.staleAfter);
-  log.debug(`Stack was last updated ${lastUpdated} hours ago and ${isStale ? 'is' : 'isn\'t'} stale`);
+  log.debug(`Stack was last updated ${lastUpdated} hours ago and ${isStale ? 'is' : "isn't"} stale`);
   return isStale;
-};
+}
 
 // Stack status is stable and not in error state
 function stackIsInDeletableStatus(stack, config): boolean {
   const isInDeletableStatus = config.deleteableStatuses.indexOf(stack.StackStatus) > -1;
-  log.debug(`Stack status is ${stack.StackStatus} which ${isInDeletableStatus ? 'is' : 'isn\'t'} deletable`);
+  log.debug(`Stack status is ${stack.StackStatus} which ${isInDeletableStatus ? 'is' : "isn't"} deletable`);
   return isInDeletableStatus;
-};
+}
 
 function publishStacksForDeletion(stacks, config) {
-  return Promise.all( stacks.map( stack => publishStackForDeletion(stack, config) ));
-};
+  return Promise.all(stacks.map(stack => publishStackForDeletion(stack, config)));
+}
 
 function publishStackForDeletion(stack, config): Promise<any> {
   const params = {
@@ -70,4 +74,4 @@ function publishStackForDeletion(stack, config): Promise<any> {
   log.debug('Publishing deletion request for stack with params', params);
   log.info(`The ${stack.StackName} stack is ready for Valhalla - informing the valkyries`);
   return sns.publish(params).promise();
-};
+}
--- stacks/delete.ts
+++ stacks/delete.ts
@@ -1,4 +1,4 @@
-import {  SNSEvent, Callback, Context, Handler } from 'aws-lambda';
+import { SNSEvent, Callback, Context, Handler } from 'aws-lambda';
 import { CloudFormation, S3 } from 'aws-sdk';
 import * as log from 'winston';
 log.configure({ level: process.env.LOG_LEVEL });
@@ -15,61 +15,65 @@ export const handler: Handler = (event: SNSEvent, context: Context, callback: Ca
     .then(getBucketsToEmpty)
     .then(emptyBuckets)
     .then(results => deleteStack(config.stack))
-    .then( stack => callback(null, `Successfully deleted stack ${stack}`) )
-    .catch( err => callback(err) );
+    .then(stack => callback(null, `Successfully deleted stack ${stack}`))
+    .catch(err => callback(err));
 };
 
 function getStackConfig(event) {
   return JSON.parse(event.Records[0].Sns.Message);
-};
+}
 
 function getStack(stackName): Promise<any> {
   const params = { StackName: stackName };
-  return cloudFormation.describeStacks(params).promise()
-          .then(res => res.Stacks[0]);
-};
+  return cloudFormation
+    .describeStacks(params)
+    .promise()
+    .then(res => res.Stacks[0]);
+}
 
 function getBucketsToEmpty(stack): Promise<any> {
   log.debug('Getting buckets to empty for stack', stack);
   let bucketsToEmpty = [];
   const cloudFormationBucketKey = process.env.BUCKETS_TO_EMPTY;
   if (stack.Outputs && stack.Outputs.length > 0) {
-    bucketsToEmpty = stack.Outputs
-      .filter( output => cloudFormationBucketKey.indexOf(output.OutputKey) > -1 )
-      .map( output => output.OutputValue );
+    bucketsToEmpty = stack.Outputs.filter(output => cloudFormationBucketKey.indexOf(output.OutputKey) > -1).map(
+      output => output.OutputValue
+    );
   }
   return Promise.resolve(bucketsToEmpty);
-};
+}
 
 function emptyBuckets(buckets): Promise<any> {
   log.debug('Emptying buckets', buckets);
   return buckets.length ? Promise.all(buckets.map(bucket => emptyBucket(bucket))) : Promise.resolve('');
-};
+}
 
 function emptyBucket(bucket) {
   log.debug('Emptying bucket', bucket);
   return listBucketObjects(bucket).then(objects => deleteObjects(objects, bucket));
-};
+}
 
 function listBucketObjects(bucket): Promise<any> {
   const params = { Bucket: bucket };
   log.debug('Listing objects with params', params);
   return s3.listObjectsV2(params).promise();
-};
+}
 
 function deleteObjects(objects, bucket): Promise<any> {
   const params = {
     Bucket: bucket,
     Delete: {
-      Objects: objects.Contents.map( object => { return { Key: object.Key } })
+      Objects: objects.Contents.map(object => {
+        return { Key: object.Key };
+      })
     }
   };
   log.debug('Deleting objects with params', params);
   return objects.Contents.length ? s3.deleteObjects(params).promise() : Promise.resolve('');
-};
+}
 
 function deleteStack(stack): Promise<any> {
   const params = { StackName: stack };
   log.debug('Deleting stack with params', params);
   return cloudFormation.deleteStack(params).promise();
-};
+}
